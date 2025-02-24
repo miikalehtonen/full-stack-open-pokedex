acdb5ea1d5dec95fb280069e751437529592ada6
Miika Lehtonen <miika.j.lehtonen@gmail.com>
2025-02-24 19:46:31 +0200

    Simulate health check failure

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const app = express();
 // get the port from env variable
 const PORT = process.env.PORT || 5000;
 
+app.get('/health', (req, res) => {
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
-
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
